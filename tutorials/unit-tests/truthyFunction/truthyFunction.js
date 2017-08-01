9aad0d69308843bac9fb915ceb67f314d90d4dba
negar <negar@binary.com>
2017-08-01 15:16:05 +0800

    make truty test pass

--- tutorials/unit-tests/truthyFunction/truthyFunction.js
+++ tutorials/unit-tests/truthyFunction/truthyFunction.js
@@ -1,1 +1,12 @@
 // Write the code to pass the test
+const truthyFunction = (() => {
+    const toBeTruthy = () => {
+        return true;
+    };
+
+    return {
+        toBeTruthy: toBeTruthy
+    };
+});
+
+module.exports = truthyFunction;
