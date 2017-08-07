d40b643d45ca43ee1864f69e151b83feb73513ff
Amin Marashi <marashi.s.amin@gmail.com>
2017-08-07 23:09:10 +0800

    Update index.js

--- tutorials/game/Goblin/index.js
+++ tutorials/game/Goblin/index.js
@@ -1,14 +1,15 @@
 //
 import Monster from '../Monster';
 
+const maxHP = 100;
+
 export default class Goblin extends Monster {
   constructor(hp) {
     super('Goblin', hp);
-    this.maxHP = hp;
   }
   getMonsterStats() {
-    if (this.hp === this.maxHP)
-      return `This is a healthy goblin with ${this.hp} health points.`
+    if (this.hp === maxHP)
+      return `This is a healthy goblin with ${maxHP} health points.`
     if (!this.hp)
       return 'This is a dead goblin.'
     return `This is a wounded goblin with ${this.hp} health points.`
