b07612f58b7609bc308f56698a15069cff8c5e39
Amin Marashi <marashi.s.amin@gmail.com>
2017-08-07 22:44:36 +0800

    Update index.js

--- tutorials/game/Battle/index.js
+++ tutorials/game/Battle/index.js
@@ -1,1 +1,10 @@
 //
+export default class Battle {
+  constructor(weapon, damage = 10) {
+    this.weapon = weapon;
+    this.damage = damage;
+  }
+  getStats() {
+    return `This is a ${this.weapon} that does 10 damage`;
+  }
+}
