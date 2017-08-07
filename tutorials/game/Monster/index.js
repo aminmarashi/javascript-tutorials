0ce63f2e7324bc8580633b9b2c267824cf012ed5
Amin Marashi <marashi.s.amin@gmail.com>
2017-08-07 22:49:34 +0800

    Update index.js

--- tutorials/game/Monster/index.js
+++ tutorials/game/Monster/index.js
@@ -1,1 +1,16 @@
 //
+export default class Monster {
+  constructor(type, hp = 100) {
+    this.type = type;
+    this.hp = hp;
+  }
+  getMonsterStats() {
+    return `This is an enemy of type ${this.type} with ${this.hp} health points.`
+  }
+  getHP() {
+    return this.hp;
+  }
+  setHP(hp) {
+    this.hp = hp;
+  }
+}
