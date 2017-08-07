ec6e741f56eecaf882ab181506b5d4c03e872e25
Amin Marashi <marashi.s.amin@gmail.com>
2017-08-07 16:42:50 +0800

    Remove game codes

--- tutorials/game/battle/index.js
+++ tutorials/game/battle/index.js
@@ -1,12 +1,1 @@
-export class Battle {
-    constructor(type, damage = 10) {
-        this.type = type;
-        this.damage = damage;
-    }
-    getStats() {
-        return `This is a ${this.type} that does ${this.damage} damage.`
-    }
-    dealDamage(enemy) {
-        enemy.setHP(+enemy.getHP() - +this.damage);
-    }
-}
+//
